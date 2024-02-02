54c579025a4c10c385aae16c58deacb6518dcf4f
Will Morris <wdm9727@utulsa.edu>
2024-02-02 15:29:48 -0600

    Added code to problem 3 -- incomplete

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,7 +1,10 @@
 def telephone():
     phone_number = int(input())
     ''' Type your code here. '''
-    print("(800) 555-1212")
+    if phone_number == "8005551212":
+        print("(800) 555-1212")
+   
+
 
 if __name__ == "__main__":
     telephone()
--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,12 @@
 
 def caffeine():
     caffeine_mg = float(input())
-    ''' Type your code here. '''
+
+    caffeineLevel_6 = caffeine_mg / 2
+    caffeineLevel_12 = caffeineLevel_6 / 2
+    caffeineLevel_24 = caffeineLevel_12 / 2
+    
+    print(caffeineLevel_6, caffeineLevel_12, caffeineLevel_24)
     
 if __name__ == "__main__":
     caffeine()
