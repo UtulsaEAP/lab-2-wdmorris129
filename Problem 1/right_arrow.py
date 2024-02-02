d7c4b45ccad4383590dee8fa6c3aa1193bc03839
Will Morris <wdm9727@utulsa.edu>
2024-02-02 15:05:12 -0600

    Added in code to build the arrow

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -1,15 +1,18 @@
 def right_arrow():
-    base_char = input()
-    head_char = input()
+    base_char = input("Input *:")
+    head_char = input("Output #:")
 
-    row1 = '      ' + head_char
+    row1 = '       ' +head_char
+    row2 = base_char + base_char +base_char +base_char +base_char +base_char +base_char + head_char + head_char
+    row3 = base_char + base_char +base_char +base_char +base_char +base_char +base_char + head_char + head_char + head_char
+    row4 = base_char + base_char +base_char +base_char +base_char +base_char +base_char + head_char
     ''' Type your code here. '''
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(row4)
 
 if __name__ == "__main__":
     right_arrow()
