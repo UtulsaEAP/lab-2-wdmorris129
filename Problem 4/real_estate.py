a371b3a6917a807538556ba35fd09f54593b4cb1
Will Morris <wdm9727@utulsa.edu>
2024-02-08 20:20:56 -0600

    Changed code formatting

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -9,8 +9,9 @@ def real_estate():
 
     # print(f'{current_price:.2f}', f'{change_price:.2f}', f'{mortage:.2f}')
 
-    print("This house is ${a}.".format (a = f'{current_price:.2f}'), "The change is ${b} since last month.".format \
-          (b = f'{change_price:.2f}'), "The estimated monthly mortgage is ${c}.".format (c = f'{mortage:.2f}'))
+    print("This house is ${a}.".format (a = f'{current_price:.2f}')\
+        , "The change is ${b} since last month.".format (b = f'{change_price:.2f}') \
+        , "The estimated monthly mortgage is ${c}.".format (c = f'{mortage:.2f}'))
    
 if __name__ == "__main__":
     real_estate()
