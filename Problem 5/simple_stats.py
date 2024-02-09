e2ed06e2fd0e15ac71a51b60a7008a1c57fa348a
Will Morris <wdm9727@utulsa.edu>
2024-02-08 20:34:56 -0600

    wrote code for problem 5

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -4,6 +4,12 @@ def simple_stats():
     num2 = float(input())
     num3 = float(input())
     num4 = float(input())
-    ''' Type your code here. '''
+
+    num_product = (num1 * num2 * num3 * num4)
+    num_avg = ((num1 + num2 + num3 + num4) / 4 )
+    
+    print(f'{num_product:.0f}', f'{num_avg:.0f}')
+    print(f'{num_product:.3f}', f'{num_avg:.3f}')
+
 if __name__ == "__main__":
     simple_stats()
