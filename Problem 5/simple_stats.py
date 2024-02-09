830403f1eb298a8ed5d1d0a77c30c58f5deb81f6
Will Morris <wdm9727@utulsa.edu>
2024-02-09 13:23:49 -0600

    Added changes to problem 5

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -1,4 +1,22 @@
+import math
 
+def simple_stats():
+    num1 = float(input())
+    num2 = float(input())
+    num3 = float(input())
+    num4 = float(input()) 
+
+    num_product = num1 * num2 * num3 * num4
+    num_avg = (num1 + num2 + num3 + num4) / 4 
+
+    print(float(num_product * 10**0) / 10**0)
+
+
+
+
+
+if __name__ == "__main__":
+    simple_stats()
 # def simple_stats():
 #     num1 = float(input())
 #     num2 = float(input())
@@ -13,20 +31,18 @@
 
 # Read input values
 # def simple_stats():
-num1 = float(input())
-num2 = float(input())
-num3 = float(input())
-num4 = float(input())
+# num1 = float(input())
+# num2 = float(input())
+# num3 = float(input())
+# num4 = float(input())
 
-    # Calculate product and average
-product = num1 * num2 * num3 * num4
-average = (num1 + num2 + num3 + num4) / 4
+#     # Calculate product and average
+# product = num1 * num2 * num3 * num4
+# average = (num1 + num2 + num3 + num4) / 4
 
-    # Output rounded integers
-print(f'{product:.0f} {average:.0f}')
+#     # Output rounded integers
+# print(f'{product:.0f} {average:.0f}')
 
-    # Output floating-point numbers with three digits after the decimal point
-print(f'{product:.3f} {average:.3f}')
+#     # Output floating-point numbers with three digits after the decimal point
+# print(f'{product:.3f} {average:.3f}')
 
-# if __name__ == "__main__":
-#     simple_stats()
